7942ceedfa8cdffb084af3b87417d80d2d5e8836
LesserPanda <less3rpanda@gmail.com>
2022-02-15 17:37:10 +0900

    채팅 추가
    Config, Controller

---
+++ tonari/src/main/java/com/tonari/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.tonari.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.tonari.handler.ChatHandler;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    }
+
+
+}

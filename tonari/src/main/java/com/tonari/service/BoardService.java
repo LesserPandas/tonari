8ad959d0dfe96d91f958b00e05b46eecc538f3ab
Najiyo <id4459@gmail.com>
2022-02-18 03:56:01 +0900

    죽어간다ㅏㅏㅏ
    팔이 너무 아파ㅏㅏㅏㅏ

--- tonari/src/main/java/com/tonari/service/BoardService.java
+++ tonari/src/main/java/com/tonari/service/BoardService.java
@@ -2,9 +2,18 @@ package com.tonari.service;
 
 import java.util.List;
 
-import com.tonari.domain.VBoardSearchVO;
+import com.tonari.domain.CategoryVO;
+import com.tonari.domain.ReviewVO;
+import com.tonari.domain.Review_viewVO;
+import com.tonari.domain.BoardSearch_viewVO;
+import com.tonari.domain.Teacherinfo_viewVO;
 
 public interface BoardService {
-	
-	public List<VBoardSearchVO> searchBoard(String orderby);
+	public List<CategoryVO> sidelist();
+	public List<BoardSearch_viewVO> searchBoard(String orderby);
+	public List<BoardSearch_viewVO> searchcategory(String category);
+	public List<BoardSearch_viewVO> searchall(String word);
+	public Teacherinfo_viewVO teacherinfo(int bno);
+	public List<Review_viewVO> review(int bno);
+	public void writeReview(ReviewVO rvo);
 }

88e9b119e293d0022cab4831ba9d526bd21b1a9c
Najiyo <id4459@gmail.com>
2022-02-24 18:39:24 +0900

    payInsert 추가

--- tonari/src/main/java/com/tonari/mapper/MyPageMapper.java
+++ tonari/src/main/java/com/tonari/mapper/MyPageMapper.java
@@ -1,6 +1,7 @@
 package com.tonari.mapper;
 
 import com.tonari.domain.MemberAuthVO;
+import com.tonari.domain.PayListVO;
 import com.tonari.domain.TeacherVO;
 import com.tonari.domain.Teacherinfo_viewVO;
 
@@ -9,4 +10,5 @@ public interface MyPageMapper {
 	public MemberAuthVO tjoinpage(String nick);
 	public void teacherjoin(TeacherVO tvo);
 	public Teacherinfo_viewVO getTeacherVO(int bno);
+	public void payInsert(PayListVO pay);
 }
